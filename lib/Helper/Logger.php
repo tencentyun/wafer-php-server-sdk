ef3033efdd51d0ced90c85d55490129808f7c0f7
miusuncle <miusuncle@gmail.com>
2016-10-11 18:00:36 +0800

    update `Helper/Logger.php`: 优化日志打印逻辑

--- lib/Helper/Logger.php
+++ lib/Helper/Logger.php
@@ -4,23 +4,39 @@ namespace QCloud_WeApp_SDK\Helper;
 class Logger {
     const SWITCH_ON = FALSE;
 
-    public static function debug($message) {
+    public static function debug(/* $message1 [, $...] */) {
         if (self::SWITCH_ON !== TRUE) {
             return;
         }
 
-        if (function_exists('log_message')) {
-            try {
+        if (!function_exists('log_message')) {
+            return;
+        }
+
+        try {
+            $numargs = func_num_args();
+            $arg_list = func_get_args();
+            $messages = array();
+
+            for ($i = 0; $i < $numargs; $i += 1) {
+                $message = $arg_list[$i];
+
                 if (is_array($message)) {
-                    $message = json_encode($message, JSON_FORCE_OBJECT);
+                    if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+                        $message = json_encode($message, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE);
+                    } else {
+                        $message = json_encode($message, JSON_FORCE_OBJECT);
+                    }
                 }
 
-                if (is_string($message)) {
-                    log_message('debug', $message);
+                if (is_string($message) || is_numeric($message)) {
+                    $messages[] = $message;
                 }
-            } catch (Exception $e) {
-                // do nothing
             }
+
+            log_message('debug', implode(' ', $messages) . "\n");
+        } catch (Exception $e) {
+            // do nothing
         }
     }
 }
