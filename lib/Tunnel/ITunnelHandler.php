426cead36d6dc97183f626488ffcbba711ed771e
miusuncle <miusuncle@gmail.com>
2016-10-12 12:21:46 +0800

    update Tunnel/ITunnelHandler.php

--- lib/Tunnel/ITunnelHandler.php
+++ lib/Tunnel/ITunnelHandler.php
@@ -4,6 +4,6 @@ namespace QCloud_WeApp_SDK\Tunnel;
 interface ITunnelHandler {
     public function onRequest($tunnelId, $userInfo);
     public function onConnect($tunnelId);
-    public function onMessage($tunnelId, $type, $message);
+    public function onMessage($tunnelId, $type, $content);
     public function onClose($tunnelId);
 }
