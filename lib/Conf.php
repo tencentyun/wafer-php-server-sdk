f6d8509ac8189a486e80b6ed32ebe40b0902e6eb
miusuncle <miusuncle@gmail.com>
2016-10-14 09:57:39 +0800

    update: lib/Conf.php

--- lib/Conf.php
+++ lib/Conf.php
@@ -3,16 +3,16 @@ namespace QCloud_WeApp_SDK;
 
 class Conf {
     // 当前使用 SDK 服务器的主机，该主机需要外网可访问
-    public static $ServerHost;
+    public static $ServerHost = '';
 
     // 鉴权服务器服务地址
-    public static $AuthServerUrl;
+    public static $AuthServerUrl = '';
 
     // 信道服务器服务地址
-    public static $TunnelServerUrl;
+    public static $TunnelServerUrl = '';
 
     // 信道服务签名密钥，该密钥需要保密
-    public static $TunnelSignatureKey;
+    public static $TunnelSignatureKey = '';
 
     public static function setup($config) {
         if (!is_array($config)) {
