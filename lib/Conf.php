ed42e2b0412157ef4cb377fcdbdecf20425191ee
miusuncle <miusuncle@gmail.com>
2016-10-17 17:42:19 +0800

    优化SDK日志输出逻辑

--- lib/Conf.php
+++ lib/Conf.php
@@ -2,6 +2,15 @@
 namespace QCloud_WeApp_SDK;
 
 class Conf {
+    //  SDK 日志输出目录
+    private static $LogPath = '';
+
+    // SDK 日志输出级别
+    private static $LogThreshold = 0;
+
+    // SDK 日志输出级别（数组）
+    private static $LogThresholdArray = array();
+
     // 当前使用 SDK 服务器的主机，该主机需要外网可访问
     private static $ServerHost = '';
 
@@ -16,11 +25,29 @@ class Conf {
 
     public static function __callStatic($name, $arguemnts) {
         $class = get_class();
-        $key = preg_replace('/^get/', '', $name);
 
-        if (property_exists($class, $key)) {
-            return self::$$key;
+        if (strpos($name, 'get') === 0) {
+            $key = preg_replace('/^get/', '', $name);
+
+            if (property_exists($class, $key)) {
+                return self::$$key;
+            }
         }
+
+        if (strpos($name, 'set') === 0) {
+            $key = preg_replace('/^set/', '', $name);
+            $value = isset($arguemnts[0]) ? $arguemnts[0] : NULL;
+
+            if (property_exists($class, $key)) {
+                if (gettype($value) === gettype(self::$$key)) {
+                    self::$$key = $value;
+                } else {
+                    throw new Exception("Call to method {$class}::{$name}() with invalid arguements", 1);
+                }
+                return;
+            }
+        }
+
 
         throw new Exception("Call to undefined method {$class}::{$name}()", 1);
     }
@@ -33,7 +60,7 @@ class Conf {
         $class = get_class();
 
         foreach ($config as $key => $value) {
-            if (property_exists($class, $key) && is_string($value)) {
+            if (property_exists($class, $key) && gettype($value) === gettype(self::$$key)) {
                 self::$$key = $value;
             }
         }
