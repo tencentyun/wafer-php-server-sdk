a9a18a605f030bd536c097ec2042255942e23851
miusuncle <miusuncle@gmail.com>
2016-10-26 18:05:49 +0800

    TunnelService::handle 第二个参数设置默认值

--- lib/Tunnel/TunnelService.php
+++ lib/Tunnel/TunnelService.php
@@ -9,7 +9,7 @@ use \QCloud_WeApp_SDK\Helper\Util as Util;
 use \QCloud_WeApp_SDK\Helper\Logger as Logger;
 
 class TunnelService {
-    public static function handle(ITunnelHandler $handler, $options) {
+    public static function handle(ITunnelHandler $handler, $options = array()) {
         if (!is_array($options)) {
             $options = array();
         }
