8ec539fefe43cf9f1b08361862531bab9b33b997
miusuncle <miusuncle@gmail.com>
2016-10-26 10:53:00 +0800

    Initialize SUT environment

---
+++ tests/Auth/LoginServiceTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use QCloud_WeApp_SDK\Helper\Util;
+
+class LoginServiceTest extends \PHPUnit_Framework_TestCase {
+    public function testHello() {
+        $this->assertTrue(TRUE, 'it works');
+    }
+}
---
+++ tests/test-server/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'Not Implemented';
